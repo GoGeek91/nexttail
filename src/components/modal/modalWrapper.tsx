49370acdbff27993d40d6374f5aa7ebc0cb6a20f
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2024-05-10 17:26:12 +0530

    fix:same modal

--- src/components/modal/modalWrapper.tsx
+++ src/components/modal/modalWrapper.tsx
@@ -49,27 +49,20 @@ const ModalWrapper = ({
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild className={cn("", modalTriggerclass)}>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger className={cn("w-full text-black", modalTriggerclass)} asChild>
         {ModalTrigger}
-      </DrawerTrigger>
-      <DrawerContent className="w-full bg-white">
-        <DrawerHeader className="border-b border-secondary-main">
-          <DrawerTitle className="">{title}</DrawerTitle>
+      </DialogTrigger>
+      <DialogContent className="xs:w-[350px] bg-white">
+        <DialogHeader className="border-b border-secondary-main">
+          <DialogTitle className="text-black">{title}</DialogTitle>
           <DialogPrimitive.Close className="transition duration-300 ease-in-out rounded-md hover:bg-secondary-default hover:bg-opacity-10 p-1 text-black">
             X<span className="sr-only">Close</span>
           </DialogPrimitive.Close>
-        </DrawerHeader>
-        <div
-          className={cn(
-            "px-3 pt-3 xl:pt-3 lg:pt-3 md:pt-3 sm:pt-3 xl:pb-3 lg:pb-3 md:pb-3 sm:pb-3 pb-6",
-            drawercontentclass
-          )}
-        >
-          {content}
-        </div>
-      </DrawerContent>
-    </Drawer>
+        </DialogHeader>
+        <div className={cn("px-5 py-5", modalcontentclass)}>{content}</div>
+      </DialogContent>
+    </Dialog>
   );
 };
 ModalWrapper.displayName = "ModalWrapper";
