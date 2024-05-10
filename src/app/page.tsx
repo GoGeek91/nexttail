86d0d6594a8e14dab287aaa05c812a15965f7f18
Suveer Grover <119030604+GoGeek91@users.noreply.github.com>
2024-05-10 17:57:25 +0530

    fix:gfdhgdha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
-
-import Image from "next/image";
-import { SettingModal } from "./modaldemo/SettingModal";
+import { MyDrawer } from "@/components/modalDrawer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SettingModal />
+      <MyDrawer />
     </main>
   );
 }
--- src/components/modal/modalWrapper.tsx
+++ src/components/modal/modalWrapper.tsx
@@ -32,7 +32,7 @@ const ModalWrapper = ({
   if (isDesktop) {
     return (
       <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger className={cn("w-full text-white", modalTriggerclass)} asChild>
+        <DialogTrigger className={cn("w-full text-black", modalTriggerclass)} asChild>
           {ModalTrigger}
         </DialogTrigger>
         <DialogContent className="xs:w-[350px] bg-white">
@@ -49,20 +49,27 @@ const ModalWrapper = ({
   }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger className={cn("w-full text-white", modalTriggerclass)} asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild className={cn("", modalTriggerclass)}>
         {ModalTrigger}
-      </DialogTrigger>
-      <DialogContent className="xs:w-[350px] bg-white">
-        <DialogHeader className="border-b border-secondary-main">
-          <DialogTitle className="text-black">{title}</DialogTitle>
+      </DrawerTrigger>
+      <DrawerContent className="w-full bg-white">
+        <DrawerHeader className="border-b border-secondary-main">
+          <DrawerTitle className="">{title}</DrawerTitle>
           <DialogPrimitive.Close className="transition duration-300 ease-in-out rounded-md hover:bg-secondary-default hover:bg-opacity-10 p-1 text-black">
             X<span className="sr-only">Close</span>
           </DialogPrimitive.Close>
-        </DialogHeader>
-        <div className={cn("px-5 py-5", modalcontentclass)}>{content}</div>
-      </DialogContent>
-    </Dialog>
+        </DrawerHeader>
+        <div
+          className={cn(
+            "px-3 pt-3 xl:pt-3 lg:pt-3 md:pt-3 sm:pt-3 xl:pb-3 lg:pb-3 md:pb-3 sm:pb-3 pb-6",
+            drawercontentclass
+          )}
+        >
+          {content}
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 ModalWrapper.displayName = "ModalWrapper";
